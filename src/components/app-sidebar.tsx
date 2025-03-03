a2a93135cbf94e8d0e5dd6ac8815d67e0790570a
irere123 <irere2020@outlook.com>
2025-03-03 14:47:07 +0200

    feat: sidebar with user info

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -5,6 +5,8 @@ import Link from 'next/link';
 import { createContext, useState, useContext, useEffect } from 'react';
 import { TooltipTrigger } from '@radix-ui/react-tooltip';
 import { useRouter } from 'next/navigation';
+import { AnimatePresence } from 'motion/react';
+import * as motion from 'motion/react-client';
 
 import {
   Astroid,
@@ -17,6 +19,7 @@ import {
 import { cn } from '@/lib/utils';
 import { Tooltip, TooltipContent, TooltipProvider } from './ui/tooltip';
 import { Avatar } from './ui/avatar';
+import { useSession } from '@/lib/auth-client';
 
 type SidebarContextType = {
   expanded: boolean;
@@ -140,7 +143,7 @@ export default function Sidebar() {
       <nav className="mt-6">
         <ul className="space-y-2 px-3">
           {menuItems.map((item) => (
-            <li key={item.label}>
+            <li key={item.label + item.path}>
               <Link
                 href={item.path}
                 className={`
@@ -158,36 +161,7 @@ export default function Sidebar() {
 
       <div className="mt-auto px-3 pb-3">
         <div className="flex items-center gap-3 rounded-md px-3 py-2">
-          <Avatar className="h-8 w-8" />
-          {expanded && (
-            <div className="flex-1 truncate">
-              <p className="truncate text-sm text-gray-300">user2050471...</p>
-            </div>
-          )}
-          {expanded && (
-            <button type="button" className="text-gray-400 hover:text-white">
-              <svg
-                className="h-5 w-5"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-              </svg>
-            </button>
-          )}
+          <SidebarUser />
         </div>
 
         {!expanded && (
@@ -202,3 +176,62 @@ export default function Sidebar() {
     </div>
   );
 }
+
+const SidebarUser: React.FC = () => {
+  const { expanded } = useSidebar();
+  const { data, isPending } = useSession();
+
+  if (isPending || !data) {
+    return null;
+  }
+
+  return (
+    <AnimatePresence>
+      <Avatar className="h-8 w-8" user={data.user} />
+      {expanded && (
+        <motion.div
+          initial={{ y: 10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: -10, opacity: 0 }}
+          key={'username'}
+          transition={{ duration: 0.3 }}
+          className="flex-1 truncate"
+        >
+          <p className="truncate text-sm text-gray-300">{data.user.name}</p>
+        </motion.div>
+      )}
+      {expanded && (
+        <motion.button
+          initial={{ y: 10, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          exit={{ y: -10, opacity: 0 }}
+          transition={{ duration: 0.3 }}
+          key={'settings'}
+          type="button"
+          className="text-neutral-400 hover:text-white"
+        >
+          <svg
+            className="h-5 w-5"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+            />
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+            />
+          </svg>
+        </motion.button>
+      )}
+    </AnimatePresence>
+  );
+};
